aa9e29ca1abaef3fb27d5c147ea3d64479f1acda
faceless49 <digital.zeus@mail.ru>
2024-01-10 18:57:26 +0300

    settings and done hw2

--- 2/webpack.config.js
+++ 2/webpack.config.js
@@ -3,6 +3,22 @@ const path = require('node:path');
 module.exports = {
 	devtool: false,
 	mode: 'development',
-
-	// ...ваш конфиг
+	entry: {
+		vendor: ['react', 'react-dom'],
+		login: {
+			import: './src/pages/login.js',
+		},
+		home: {
+			import: './src/pages/home.js',
+			dependOn: 'vendor',
+		},
+		app: {
+			import: './src/app.js',
+			dependOn: 'vendor',
+		},
+	},
+	output: {
+		filename: "[name].[contenthash].js",
+		path: path.resolve(__dirname, './build'),
+	}
 }
