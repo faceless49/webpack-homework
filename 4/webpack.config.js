b92cb5849d69874b067b366f9cd579b3f3605c3a
faceless49 <digital.zeus@mail.ru>
2024-01-12 18:02:18 +0300

    HW-4 done (resolve, alias)

--- 4/webpack.config.js
+++ 4/webpack.config.js
@@ -12,6 +12,26 @@ module.exports = {
 			}
 		]
 	},
-
+	entry: {
+		app: {
+			import: './src/app.js',
+		},
+	},
+	output: {
+		path: path.resolve(__dirname, './bundle'),
+		filename: 'awesome-bundle.js'
+	},
+	resolve: {
+		extensions: ['...', '.cooljson',],
+		alias: {
+			Components: path.resolve(__dirname, 'src/components'),
+			_: [path.resolve(__dirname, 'src/helpers'), path.resolve(__dirname, 'src/utilities')],
+			Helpers: path.resolve(__dirname, 'src/helpers'),
+			Utils: path.resolve(__dirname, 'src/utilities'),
+		},
+		fallback: {
+			coolurl: path.resolve(__dirname, 'src/utilities/url.js'),
+		}
+	}
 	// ...ваш конфиг
 }
